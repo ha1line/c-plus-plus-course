526c5b1344ff18ef630b9b42d490878af3be0745
Matevosyan Artem <54016265+Crisnit@users.noreply.github.com>
2023-10-02 21:01:56 +0500

    Add files via upload

---
+++ Tasks/IsPalindrom/IsPalindrom.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+bool IsPalindrome(const std::string& str)
+{
+    int rightBorder = 0;
+    for (size_t i = 0; i < str.length()/2; i++)
+    {
+        if (str[i]!=str[str.length()-1-rightBorder])
+        {
+            return false;
+        }
+        rightBorder++;
+    }
+    return true;
+}
+
+int main()
+{
+    std::string _str="ababab";
+    std::cout<< IsPalindrome(_str);
+}
