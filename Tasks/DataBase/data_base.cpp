20c148f2593805d75e4de0e83c91fad04eb892ba
Rtemmm <yadolbaeb30@gmail.com>
2023-12-14 18:19:41 +0500

    solution

--- Tasks/DataBase/data_base.cpp
+++ Tasks/DataBase/data_base.cpp
@@ -1,28 +1,34 @@
-// вы можете подключить все необходимые вам библиотеки из стандарта
 #include <iostream>
 #include <vector>
-#include <string>
 #include <map>
-#include <algorithm>
-#include <iomanip>
 #include <set>
-#include <exception>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
 
-// нужно заполнить методы и перегрузить нужные операторы
 class Date
 {
 public:
-    Date(int year, int month, int day) : 
-        m_year(year),
-        m_month(month),
-        m_day(day)
-    {}
+    Date(int year, int month, int day) : m_year(year),
+                                         m_month(month),
+                                         m_day(day)
+    {
+        if (month < 1 || month > 12)
+        {
+            throw std::runtime_error("Month value is invalid: " + std::to_string(month));
+        }
+
+        if (day < 1 || day > 31)
+        {
+            throw std::runtime_error("Day value is invalid: " + std::to_string(day));
+        }
+    }
 
     int GetYear() const
     {
         return m_year;
     }
-    
+
     int GetMonth() const
     {
         return m_month;
@@ -33,126 +39,94 @@ public:
         return m_day;
     }
 
-    static Date ParseDate(std::istringstream& is)
+    static Date ParseDate(std::istringstream &is)
     {
         int year, month, day;
+        char sep1, sep2;
 
         std::string date;
-        is >> std::ws;
         std::getline(is, date, ' ');
 
         std::istringstream stream(date);
 
-        if (!isdigit(date[0]) || !isdigit(date.back()))
+        if (!(stream >> year >> sep1 >> month >> sep2 >> day))
         {
             throw std::runtime_error("Wrong date format: " + date);
         }
 
-        stream >> year;
-        CheckSeparator(stream, date);
-
-        stream >> month;
-        if (month < 1 || month > 12)
+        else if (sep1 == '-' && sep2 == '-' && stream.get() == EOF)
         {
-            throw std::runtime_error("Month value is invalid: " + std::to_string(month));
+            return Date(year, month, day);
         }
 
-        CheckSeparator(stream, date);
-
-        stream >> day;
-        if (day < 1 || day > 31)
+        else
         {
-            throw std::runtime_error("Day value is invalid: " + std::to_string(day));
+            throw std::runtime_error("Wrong date format: " + date);
         }
-
-        return Date(year, month, day);
     }
+
 private:
     int m_year;
     int m_month;
     int m_day;
-
-    static void CheckSeparator(std::istringstream& stream, const std::string& date)
-    {
-        if (stream.get() != '-')
-        {
-            throw std::runtime_error("Wrong date format: " + date);
-        }
-    }
 };
 
-// здесь советую воспользоваться трюком с вектором из лекции, либо std::tie
-bool operator<(const Date& lhs, const Date& rhs)
+bool operator<(const Date &lhs, const Date &rhs)
 {
-    return std::vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} < 
-        std::vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return std::vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} <
+           std::vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
 }
 
 class DataBase
 {
 public:
-    void AddEvent(const Date& date, const std::string& event)
+    void AddEvent(const Date &date, const std::string &event)
     {
-        if (m_db.find(date) != m_db.end())
-        {
-            m_db[date].insert(event);
-        }
-        else
-        {
-            m_db[date] = std::set<std::string>{event};
-        }
+        m_db[date].insert(event);
     }
 
-    bool Delete(const Date& date, const std::string& event)
+    bool Delete(const Date &date, const std::string &event)
     {
-        auto iterator = m_db.find(date);
-        if (iterator == m_db.end())
+        if (m_db.contains(date) && m_db[date].contains(event))
         {
-            return false;
+            m_db[date].erase(event);
+            return true;
         }
 
-        std::set<std::string>& events = iterator->second;
-
-        auto eventIterator = std::find(events.begin(), events.end(), event);
-        if (eventIterator == events.end())
-        {
-            return false;
-        }
-
-        events.erase(eventIterator);
-
-        if (events.empty())
-        {
-            m_db.erase(iterator);
-        }
-
-        return true;
+        return false;
     }
 
-    int Delete(const Date& date)
+    int Delete(const Date &date)
     {
-        int eventsCount = m_db[date].size();
-        m_db.erase(date);
+        if (m_db.contains(date))
+        {
+            int deleteCount = m_db[date].size();
+            m_db.erase(date);
 
-        return eventsCount;
+            return deleteCount;
+        }
+
+        return 0;
     }
-    
-    std::set<std::string> Find(const Date& date)
-    {
-        return m_db[date];
+
+    std::set<std::string> Find(const Date &date)
+    { 
+        return m_db[date]; 
     }
 
     void Print() const
     {
-        for (const auto& pair : m_db)
-        for (const auto& event : pair.second)
+        for (const auto &pair : m_db)
         {
-            std::cout << std::setw(4) << std::setfill('0');
-            std::cout << pair.first.GetYear() << '-';
-            std::cout << std::setw(2) << std::setfill('0');
-            std::cout << pair.first.GetMonth() << '-';
-            std::cout << std::setw(2) << std::setfill('0');
-            std::cout << pair.first.GetDay() << ' ' << event << std::endl;
+            for (const auto &event : pair.second)
+            {
+                std::cout << std::setw(4) << std::setfill('0');
+                std::cout << pair.first.GetYear() << '-';
+                std::cout << std::setw(2) << std::setfill('0');
+                std::cout << pair.first.GetMonth() << '-';
+                std::cout << std::setw(2) << std::setfill('0');
+                std::cout << pair.first.GetDay() << ' ' << event << std::endl;
+            }
         }
     }
 
@@ -174,12 +148,13 @@ int main()
 
         try
         {
+            is >> std::ws;
+
             if (command == "Add")
             {
                 Date date = Date::ParseDate(is);
-                std::string event;
 
-                is >> std::ws;
+                std::string event;
                 is >> event;
 
                 db.AddEvent(date, event);
@@ -193,18 +168,18 @@ int main()
 
                 if (!event.empty())
                 {
-                    if (!db.Delete(date, event))
+                    if (db.Delete(date, event))
                     {
-                        std::cout << "Event not found" << std::endl;
+                        std::cout << "Deleted successfully" << std::endl;
                     }
                     else
                     {
-                        std::cout << "Deleted successfully" << std::endl;
+                        throw std::runtime_error("Event not found");
                     }
                 }
                 else
                 {
-                    std::cout << "Deleted "<< db.Delete(date) << " events" << std::endl;
+                    std::cout << "Deleted " << db.Delete(date) << " events" << std::endl;
                 }
             }
             else if (command == "Find")
@@ -212,7 +187,7 @@ int main()
                 Date date = Date::ParseDate(is);
                 std::set<std::string> events = db.Find(date);
 
-                for (const auto& event : events)
+                for (const auto &event : events)
                 {
                     std::cout << event << std::endl;
                 }
@@ -221,7 +196,7 @@ int main()
             {
                 db.Print();
             }
-            else if (command == "")
+            else if (command.empty())
             {
                 continue;
             }
@@ -230,12 +205,10 @@ int main()
                 throw std::runtime_error("Unknown command: " + command);
             }
         }
-        catch (const std::runtime_error& e)
+        catch (const std::runtime_error &e)
         {
-            std::cerr << e.what() << std::endl;
-            //return 1;
+            std::cout << e.what() << std::endl;
         }
-
     }
 
     return 0;
